e011a88eeacbe26dbba096c85dac610420679e5c
koki.nakashima <kurikko19@gmail.com>
2021-02-02 21:10:56 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 中島晃希 on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
+
+
+}
+
