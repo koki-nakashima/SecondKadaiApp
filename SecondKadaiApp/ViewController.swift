50b44eed4be761b1cb556211611fc89f6f957435
koki.nakashima <kurikko19@gmail.com>
2021-02-03 18:12:37 +0900

    SecondKadaiAppのラベルを追加

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var inputField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        
+            let next = segue.destination as? ResultViewController
+            next?.outputValue = self.inputField.text
+    }
+    
+    @IBAction func tapTransitionButton(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toResultViewController", sender: nil)
+    }
+    
+    @IBAction func backToTitle(sender: UIStoryboardSegue) {
+        
+    }
 
 
 }
